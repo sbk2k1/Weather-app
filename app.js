f1c31e00a1f35491011dd41ef83d1652aa6eedd2
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-10-03 11:56:47 +0530

    Weather App

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-console.log('Starting the program')
-
-setTimeout(() => {
- console.log('asynchronous 2 sec timer')
-}, 2000)
-//weatherstack.com
-console.log('Stopping')
-
-console.log('Stopping the program')
+const request = require('request');
