2f53abc25e68bf9f886dcb7031021aa5e3204a84
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-10-03 12:16:13 +0530

    HTTP Request

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const request = require('request');
+
+const url= 'http://api.weatherstack.com/current?access_key=5ffe734b37519e030e693326e95e7747&query=37.8267,-122.4233'
+
+request({ url: url, json: true }, (error, response) => {
+ console.log(`It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+})
