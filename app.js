6a6b223a200179a3cfeb2e67f87af0755602eee7
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-10-03 16:09:40 +0530

    Geocoding

--- app.js
+++ app.js
@@ -11,4 +11,5 @@ const geocodeURL= 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angel
 request({ url: geocodeURL, json: true}, (error, response) => {
  const latitude= response.body.features[0].center[1]
  const longitude= response.body.features[0].center[0]
+ console.log(latitude, longitude)
 })
