b00e4939c0106988d1bcf7bf7f8ec2187ed6419b
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-10-09 18:50:11 +0530

    User input through command line

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const geocode= require('./utils/geocode')
-const forecast= require('./utils/forecast')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 
-//hard-coded to Boston
+const address = process.argv[2]
 
-geocode('Boston', (error, data) => {
+if (!address) {
+ console.log('Please provide an address')
+}
+else {
+ geocode(address, (error, data) => {
 
- if(error){
-  return console.log(error)
- }
-
- forecast(data.latitude, data.longitude, (error, forecastData) => {
-  if(error){
+  if (error) {
    return console.log(error)
   }
-  console.log(data.location);
-  console.log(forecastData.data);
+
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+   if (error) {
+    return console.log(error)
+   }
+   console.log(data.location);
+   console.log(forecastData.data);
+  })
  })
-})
+}
+
